012af8d88268efabc723df0daf5c609dfef4366e
jfsom <vijay.jfsom@gmail.com>
2025-03-19 22:00:38 +0530

    Creating SqlConnection Instance Using the Constructor with Connection String

--- ADODOTNETCoreDemo/Program.cs
+++ ADODOTNETCoreDemo/Program.cs
@@ -9,34 +9,15 @@ namespace ADODOTNETCoreDemo
             {
                 //I am using Windows Authentication and hence no need to pass the User Id and Password
                 string connectionString = "Server=DESKTOP-RUC57UF;Database=StudentDB;Trusted_Connection=True;TrustServerCertificate=True;";
-                // SQL query to delete a record
-                string sqlQuery = "DELETE FROM Students WHERE Id = @Id";
-
-                //While Creating the SqlConnection passing the Connection String
+                //Using SqlConnection Constructor which takes connectionString as a parameter
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     //Open the Connection
                     connection.Open();
 
-                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
-                    {
-                        // Replace @Id with the actual id of the record you want to delete
-                        command.Parameters.AddWithValue("@Id", 1);
-
-                        //Execute the Delete Query
-                        int result = command.ExecuteNonQuery();
-
-                        // Check if the delete operation was successful
-                        if (result > 0)
-                        {
-                            Console.WriteLine("Record Deleted Successfully.");
-                        }
-                        else
-                        {
-                            Console.WriteLine("No Record Found with the Specified Id.");
-                        }
-                    }
-                }
+                    //Use the Connection
+                    Console.WriteLine("Connection Established Successfully");
+                } //Automatically using block close the connection even if exception has been occurred
             }
             catch (Exception ex)
             {
