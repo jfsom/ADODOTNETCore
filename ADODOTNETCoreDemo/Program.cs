298b52f59a6f88b81f6fe4dc5e6874dcb1531108
jfsom <vijay.jfsom@gmail.com>
2025-03-19 22:07:13 +0530

    Explicitly Close the SqlConnection

--- ADODOTNETCoreDemo/Program.cs
+++ ADODOTNETCoreDemo/Program.cs
@@ -1,27 +1,36 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Data;
 namespace ADODOTNETCoreDemo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Connection String
+            string connectionString = "Server=DESKTOP-RUC57UF;Database=StudentDB;Trusted_Connection=True;TrustServerCertificate=True;";
+            //Creating an Instance of SqlConnection Object
+            SqlConnection connection = new SqlConnection(connectionString);
             try
             {
-                //Connection String
-                string connectionString = "Server=DESKTOP-RUC57UF;Database=StudentDB;Trusted_Connection=True;TrustServerCertificate=True;";
-                //Creating an Instance of SqlConnection using the using statement
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    //Open the Connection
-                    connection.Open();
+                //Open the Connection
+                connection.Open();
+                Console.WriteLine("Connection Established Successfully");
 
-                    //Use the Connection
-                    Console.WriteLine("Connection Established Successfully");
-                } //Automatically close the connection even if an exception is raised
+                // Perform database operations
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Something Went Wrong: {ex.Message}");
+            }
+            finally
+            {
+                // Ensure the connection is closed even if an exception occurs
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                    Console.WriteLine("Connection is Closed");
+                }
+                connection.Dispose();
             }
         }
     }
