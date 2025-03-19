d5c817cc26e2617e6d265809737041272c1d05a2
jfsom <vijay.jfsom@gmail.com>
2025-03-19 21:39:06 +0530

    Retrieve All Records using ADO.NET Core from SQL Server Database

--- ADODOTNETCoreDemo/Program.cs
+++ ADODOTNETCoreDemo/Program.cs
@@ -9,13 +9,8 @@ namespace ADODOTNETCoreDemo
             {
                 //I am using Windows Authentication and hence no need to pass the User Id and Password
                 string connectionString = "Server=DESKTOP-RUC57UF;Database=StudentDB;Trusted_Connection=True;TrustServerCertificate=True;";
-                // Example student data to insert
-                string studentFirstName = "Pranaya";
-                string studentLastName = "Rout";
-                string studentEmail = "Pranaya@Example.com";
-
-                // SQL INSERT statement
-                string insertSql = "INSERT INTO Students (FirstName, LastName, Email) VALUES (@FirstName, @LastName, @Email)";
+                // SQL query to retrieve all records
+                string sqlQuery = "SELECT * FROM Students";
 
                 //While Creating the SqlConnection passing the Connection String
                 using (SqlConnection connection = new SqlConnection(connectionString))
@@ -23,22 +18,15 @@ namespace ADODOTNETCoreDemo
                     //Open the Connection
                     connection.Open();
 
-                    //Create the Command Objecr
-                    using (SqlCommand command = new SqlCommand(insertSql, connection))
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
                     {
-                        // Add parameters to prevent SQL injection
-                        command.Parameters.AddWithValue("@FirstName", studentFirstName);
-                        command.Parameters.AddWithValue("@LastName", studentLastName);
-                        command.Parameters.AddWithValue("@Email", studentEmail);
-
-                        //Execute the Coomand
-                        int result = command.ExecuteNonQuery();
-
-                        // Check the result
-                        if (result < 0)
-                            Console.WriteLine("Error Inserting Data Into Database!");
-                        else
-                            Console.WriteLine("Data Inserted Successfully!");
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine($"Id: {reader["Id"]}, First Name: {reader["FirstName"]}, Last Name: {reader["LastName"]}, Email: {reader["Email"]}");
+                            }
+                        }
                     }
                 }
             }
