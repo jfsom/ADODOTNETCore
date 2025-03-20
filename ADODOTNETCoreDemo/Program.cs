fb8dc71a0d6fea502460ace98209a5dfd2376349
jfsom <vijay.jfsom@gmail.com>
2025-03-20 15:03:12 +0530

    [200~Constructor with Command Text and Connection: SqlCommand(string cmdText, Microsoft.Data.SqlClient.SqlConnection connection)~

--- ADODOTNETCoreDemo/Program.cs
+++ ADODOTNETCoreDemo/Program.cs
@@ -10,15 +10,16 @@ namespace ADODOTNETCoreDemo
                 string connectionString = "Server=DESKTOP-RUC57UF;Database=EmployeeDB;Trusted_Connection=True;TrustServerCertificate=True;";
                 string sqlQuery = "SELECT COUNT(*) FROM Employee";
 
-                using (SqlCommand command = new SqlCommand(sqlQuery))
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    command.Connection = new SqlConnection(connectionString);
-                    command.Connection.Open();
-
-                    int count = (int)command.ExecuteScalar();
-
-                    Console.WriteLine($"Total Employees: {count}");
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        int count = (int)command.ExecuteScalar();
+                        Console.WriteLine($"Total Employees: {count}");
+                    }
                 }
+                Console.ReadLine();
             }
             catch (Exception ex)
             {
