5524845bc766fea7c187c50b0e22e9cb843c8d56
jfsom <vijay.jfsom@gmail.com>
2025-03-20 14:42:01 +0530

    ADO.NET Core Asynchronous SqlConnection

--- ADODOTNETCoreDemo/Program.cs
+++ ADODOTNETCoreDemo/Program.cs
@@ -1,45 +1,23 @@
 ﻿using Microsoft.Data.SqlClient;
-//Step1: Import the following Namespace
-using Microsoft.Extensions.Configuration;
 namespace ADODOTNETCoreDemo
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            string connectionString = "Server=DESKTOP-RUC57UF;Database=StudentDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
             try
             {
-                //Get the Connection String from appsettings.json file
-
-                //Step2: Load the Configuration File.
-                var configBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-
-                // Step3: Get the Section to Read from the Configuration File
-                var configSection = configBuilder.GetSection("ConnectionStrings");
-
-                // Step4: Get the Configuration Values based on the Config key.
-                var connectionString = configSection["SQLServerConnection"] ?? null;
-
-                if (connectionString != null)
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
-                    //Creating an Instance of SqlConnection using the using statement
-                    using (SqlConnection connection = new SqlConnection(connectionString))
-                    {
-                        //Open the Connection
-                        connection.Open();
-                        Console.WriteLine("Connection Established Successfully");
-
-                        //Perform the Database Operations
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Connection String is Missing");
+                    await connection.OpenAsync();
+                    Console.WriteLine("Connection opened successfully.");
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Something Went Wrong: {ex.Message}");
+                Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
     }
