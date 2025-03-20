0cf8b91b35c2f681cc98ab0d7eb544687fd5152a
jfsom <vijay.jfsom@gmail.com>
2025-03-20 14:57:12 +0530

    Different Ways to Create an Instance of SqlCommand object in ADO.NET Core

--- ADODOTNETCoreDemo/Program.cs
+++ ADODOTNETCoreDemo/Program.cs
@@ -7,46 +7,18 @@ namespace ADODOTNETCoreDemo
         {
             try
             {
-                //I am using Windows Authentication and hence no need to pass the User Id and Password
                 string connectionString = "Server=DESKTOP-RUC57UF;Database=EmployeeDB;Trusted_Connection=True;TrustServerCertificate=True;";
 
-                // Query to Insert a New Employee using ExecuteNonQuery
-                string sqlQuery = @"INSERT INTO Employee (FirstName, LastName, Email, Position, Salary) VALUES (@FirstName, @LastName, @Email, @Position, @Salary)";
-
-                //Create an Instance of SqlConnection
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlCommand command = new SqlCommand())
                 {
-                    connection.Open();
+                    command.CommandText = "SELECT COUNT(*) FROM Employee";
+                    command.Connection = new SqlConnection(connectionString);
+                    command.Connection.Open();
 
-                    // ExecuteNonQuery Example
-                    Console.WriteLine("ExecuteNonQuery Example");
-                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
-                    {
-                        // Assuming these variables are set from your application's input
-                        string firstName = "Priyanka";
-                        string lastName = "Dewangan";
-                        string email = "Priyanka@Example.com";
-                        string position = "Software Developer";
-                        decimal salary = 75000m;
+                    int count = (int)command.ExecuteScalar();
 
-                        command.Parameters.AddWithValue("@FirstName", firstName);
-                        command.Parameters.AddWithValue("@LastName", lastName);
-                        command.Parameters.AddWithValue("@Email", email);
-                        command.Parameters.AddWithValue("@Position", position);
-                        command.Parameters.AddWithValue("@Salary", salary);
-
-                        int result = command.ExecuteNonQuery();
-                        if (result > 0)
-                        {
-                            Console.WriteLine("Employee Added Successfully.");
-                        }
-                        else
-                        {
-                            Console.WriteLine("No Employee Was Added.");
-                        }
-                    } //Command Object will be disposed automatically
-
-                } //Connection Object will be disposed automatically
+                    Console.WriteLine($"Total Employees: {count}");
+                }
             }
             catch (Exception ex)
             {
